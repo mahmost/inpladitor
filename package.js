b382509bb9def04cb3b70b1bef99e12a793c0b34
Mahmoud Mostafa <mah@moud.info>
2018-08-30 17:34:28 +0200

    Minor changes in package.js (also added .eslintignore file)

--- package.js
+++ package.js
@@ -1,15 +1,16 @@
 Package.describe({
-  name: "mahmost:inpladitor",
-  git: "https://github.com/mahmost/inpladitor.git",
-  summary: "An in-place editor for meteor",
-  version: "0.1.0"
+  name: 'mahmost:inpladitor',
+  git: 'https://github.com/mahmost/inpladitor.git',
+  summary: 'An in-place editor for meteor',
+  version: '0.1.0',
 });
 
 Npm.depends({
-  "optimal-select": '3.5.0'
+  'optimal-select': '3.5.0',
+  loglevel: '1.6.1',
 });
 
-Package.onUse(function (api) {
+Package.onUse((api) => {
   api.versionsFrom('1.4.3.2');
 
   api.use('modules');
@@ -18,5 +19,4 @@ Package.onUse(function (api) {
   api.mainModule('model.js', 'server');
 
   api.addFiles('css/style.css', 'client');
-
 });
